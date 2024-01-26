7b7fef5a4b202336ff089a102749ef7852fc699c
Alina Peric <129123581+Alinaperic@users.noreply.github.com>
2024-01-26 00:00:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let currentInput = '';
+
+function appendSymbol(symbol) {
+  currentInput += symbol;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentInput = '';
+  updateDisplay();
+}
+
+function deleteLast() {
+  currentInput = currentInput.slice(0, -1);
+  updateDisplay();
+}
+
+function calculate() {
+  try {
+    const result = eval(currentInput);
+    currentInput = result.toString();
+    updateDisplay();
+  } catch (error) {
+    currentInput = 'Error';
+    updateDisplay();
+  }
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = currentInput;
+}
